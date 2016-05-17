115088e418f7c3f2e6da69cc78e0f377fd4901b4
chanhy <tnycit1993@gmail.com>
2016-05-17 11:21:42 +0700

    add

--- app/src/main/java/com/example/chanhy/cuisine/LoginActivity.java
+++ app/src/main/java/com/example/chanhy/cuisine/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.chanhy.cuisine;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -11,25 +14,31 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+
 /**
  * Created by chanhy on 12/05/2016.
  */
 public class LoginActivity extends ActionBarActivity {
 
+    final Context context = this;
+    LinearLayout logo, llemail, llpassword;
+    Button signup, mBtnSignup, signin, forgot;
+    ImageView tech, asian;
+    TextView welcome;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ImageView tech = (ImageView) findViewById(R.id.imgTech);
-        ImageView asian = (ImageView) findViewById(R.id.imgAsian);
-        TextView welcome = (TextView) findViewById(R.id.tvWelcome);
-        Button forgot = (Button) findViewById(R.id.btninForgot);
-        Button signup = (Button) findViewById(R.id.btninSignup);
-        Button signin = (Button) findViewById(R.id.btnSignIn);
-        LinearLayout logo = (LinearLayout) findViewById(R.id.lnImgLogo);
-        LinearLayout llemail = (LinearLayout) findViewById(R.id.llEmail);
-        LinearLayout llpassword = (LinearLayout) findViewById(R.id.llPassword);
+        tech = (ImageView) findViewById(R.id.imgTech);
+        asian = (ImageView) findViewById(R.id.imgAsian);
+        welcome = (TextView) findViewById(R.id.tvWelcome);
+        forgot = (Button) findViewById(R.id.btninForgot);
+        signup = (Button) findViewById(R.id.btninSignup);
+        signin = (Button) findViewById(R.id.btnSignIn);
+        logo = (LinearLayout) findViewById(R.id.lnImgLogo);
+        llemail = (LinearLayout) findViewById(R.id.llEmail);
+        llpassword = (LinearLayout) findViewById(R.id.llPassword);
 
         final Animation animAsian = AnimationUtils.loadAnimation(this, R.anim.anim_asian);
         final  Animation animTech = AnimationUtils.loadAnimation(this, R.anim.anim_tech);
@@ -47,21 +56,41 @@ public class LoginActivity extends ActionBarActivity {
         signin.startAnimation(animup);
         forgot.startAnimation(animup);
 
-        signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent1 = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent1);
-            }
-        });
+       signin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+               startActivity(intent);
+           }
+       });
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(intent2);
+                final Dialog dialog = new Dialog(LoginActivity.this);
+
+                dialog.getWindow();
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.activity_signup);
+                dialog.getWindow().getAttributes().windowAnimations = R.style.DialogTheme;
+                dialog.show();
             }
         });
+
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialogforgot = new Dialog(LoginActivity.this);
+
+                dialogforgot.getWindow();
+                dialogforgot.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialogforgot.setContentView(R.layout.activity_forgot);
+                dialogforgot.getWindow().getAttributes().windowAnimations = R.style.DialogTheme;
+                dialogforgot.show();
+            }
+        });
+
     }
+
 
 }
