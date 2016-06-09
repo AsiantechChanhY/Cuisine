47da3a81cac4bde7c2c217ea5d418ccd69a29e3b
chanhy <tnycit1993@gmail.com>
2016-06-09 14:03:06 +0700

    add

---
+++ app/src/main/java/com/example/chanhy/cuisine/Model/AndroidVersion.java
@@ -0,0 +1,53 @@
+package com.example.chanhy.cuisine.Model;
+
+/**
+ * Created by chanhy on 23/05/2016.
+ */
+public class AndroidVersion {
+
+    private String name;
+    private String userId;
+    private String datetime;
+    private String location;
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
